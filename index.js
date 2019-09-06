0ee5af3ff5190fe2ccc18bbda6f975778c0a281e
Manikandan <tsmanikandan@protonmail.com>
2019-09-06 17:13:54 -0400

    Extract data from sudoku.json and create a Map of squares to their units

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const chalk = require('chalk');
+const constants = require('./sudoku.json');
 
-process.stdout.write(chalk.cyan.bold('Yet Another Sudoku Solver'));
+console.log(chalk.cyan.bold('Yet Another Sudoku Solver\n'));
+const { squares, unitlist } = constants;
+
+const cross = (A, B) => {
+	const crossProduct = new Array();
+	for (const i of A) {
+		for (const j of B) {
+			crossProduct.push(i + j);
+		}
+	}
+	return crossProduct;
+}
+
+const units = new Map();
+let curUnits = [];
+for (const s of squares) {
+	// gets all unitlist arrays that contain s
+	curUnits = unitlist.filter(u => u.includes(s));
+	units.set(s, curUnits);
+}
+
+// CommonJS
+module.exports = {
+	squares,
+	unitlist,
+	units
+}
