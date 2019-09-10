eabc4bc89b0b785f129d836cfa15de5e7762c0e2
Manikandan <tsmanikandan@protonmail.com>
2019-09-09 22:28:37 -0400

    Add ts config and move index.js to src folder

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import chalk from 'chalk';
+import constants from '../sudoku.json';
+
+console.log(chalk.cyan.bold('Yet Another Sudoku Solver\n'));
+const { squares, unitlist } = constants;
+
+
+const digits: string   = '123456789'
+const rows: 	string   = 'ABCDEFGHI'
+const cols:		string   = digits
+const units = new Map();
+const peers = new Map();
+
+let curUnits = [];
+for (const s of squares) {
+	// gets all unitlist arrays that contain s
+	curUnits = unitlist.filter(u => u.includes(s));
+	units.set(s, curUnits);
+
+	const arrReducer = (acc, cur) => acc.concat(cur);
+	peers.set(s, new Set(curUnits.reduce(arrReducer)));
+}
+
+const gridValues = (grid: string) => {
+	const chars: [string, string][] = [];
+	grid.split('').forEach((v, i) => {
+		if (digits.includes(v) || '0.'.includes(v)){
+			chars.push([squares[i],v]);
+		}
+	});
+	return new Map(chars);
+}
+
+const parseGrid = (grid) => {
+	// Convert grids to a Map of possible values
+	const values = new Map();
+	squares.map(s => {values.set(s, digits)});
+	for (const [s, d] of gridValues(grid)){
+		if (digits.includes(d) && !assign(values, s, d)){
+			return false;
+		}
+	}
+	return values;
+}
+
+// CommonJS
+module.exports = {
+	squares,
+	unitlist,
+	units,
+	peers,
+	gridValues,
+	parseGrid
+}
