287385f5858c9a1fc6b3919b01a45de39ac90ddf
Manikandan <tsmanikandan@protonmail.com>
2019-09-10 10:04:14 -0400

    Convert random functions and loops into one exportable class

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,48 @@
 import chalk from 'chalk';
 import constants from '../sudoku.json';
 
-console.log(chalk.cyan.bold('Yet Another Sudoku Solver\n'));
-const { squares, unitlist } = constants;
+export default class Yass {
+	digits = '123456789';
+	rows = 'ABCDEFGHI';
+	cols = this.digits;
+	units: Map<string, string[][]> = new Map();
+	peers: Map<string, Set<string>> = new Map();
+	squares: string[] = constants.squares;
+	unitlist: string[][] = constants.unitlist;
 
-const digits = '123456789';
-// const rows = 'ABCDEFGHI';
-// const cols: string = digits;
-const units: Map<string, string[][]> = new Map();
-const peers: Map<string, Set<string>> = new Map();
+	constructor() {
+		let curUnits: string[][] = [];
+		for (const s of this.squares) {
+			// gets all unitlist arrays that contain s
+			curUnits = this.unitlist.filter(u => u.includes(s));
+			this.units.set(s, curUnits);
 
-let curUnits: string[][] = [];
-for (const s of squares) {
-	// gets all unitlist arrays that contain s
-	curUnits = unitlist.filter(u => u.includes(s));
-	units.set(s, curUnits);
-
-	const arrReducer = (acc: string[], cur: string[]) => {
-		return acc.concat(cur);
-	};
-	peers.set(s, new Set(curUnits.reduce(arrReducer)));
-}
-
-const gridValues = (grid: string) => {
-	const chars: [string, string][] = [];
-	grid.split('').forEach((v, i) => {
-		if (digits.includes(v) || '0.'.includes(v)) {
-			chars.push([squares[i], v]);
+			const arrReducer = (acc: string[], cur: string[]) => {
+				return acc.concat(cur);
+			};
+			this.peers.set(s, new Set(curUnits.reduce(arrReducer)));
 		}
-	});
-	return new Map(chars);
-};
+	}
 
-// export const parseGrid = (grid: string) => {
-// 	// Convert grids to a Map of possible values
-// 	const values: Map<string, string> = new Map();
-// 	squares.map(s => {values.set(s, digits)});
-// 	gridValues(grid).forEach((v, k) => {
-// 		if (digits.includes(v) && !assign(values, v, k)){
-// 			return false;
-// 		}
-// 	});
-// 	return values;
-// }
+	gridValues = (grid: string) => {
+		const chars: [string, string][] = [];
+		grid.split('').forEach((v, i) => {
+			if (this.digits.includes(v) || '0.'.includes(v)) {
+				chars.push([this.squares[i], v]);
+			}
+		});
+		return new Map(chars);
+	};
 
-export { squares, unitlist, units, peers, gridValues };
+	// export const parseGrid = (grid: string) => {
+	// 	// Convert grids to a Map of possible values
+	// 	const values: Map<string, string> = new Map();
+	// 	squares.map(s => {values.set(s, digits)});
+	// 	gridValues(grid).forEach((v, k) => {
+	// 		if (digits.includes(v) && !assign(values, v, k)){
+	// 			return false;
+	// 		}
+	// 	});
+	// 	return values;
+	// }
+}
