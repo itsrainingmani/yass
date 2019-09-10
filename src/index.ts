dbf543f33245fe89de748f778f902d175fb330fa
Manikandan <tsmanikandan@protonmail.com>
2019-09-09 22:40:32 -0400

    Add more type annotations

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,16 @@ const { squares, unitlist } = constants;
 const digits: string   = '123456789'
 const rows: 	string   = 'ABCDEFGHI'
 const cols:		string   = digits
-const units = new Map();
-const peers = new Map();
+const units: Map<string, string[][]>  = new Map();
+const peers: Map<string, Set<string>> = new Map();
 
-let curUnits = [];
+let curUnits: string[][] = [];
 for (const s of squares) {
 	// gets all unitlist arrays that contain s
 	curUnits = unitlist.filter(u => u.includes(s));
 	units.set(s, curUnits);
 
-	const arrReducer = (acc, cur) => acc.concat(cur);
+	const arrReducer = (acc: string[], cur: string[]) => acc.concat(cur);
 	peers.set(s, new Set(curUnits.reduce(arrReducer)));
 }
 
@@ -31,7 +31,7 @@ const gridValues = (grid: string) => {
 	return new Map(chars);
 }
 
-const parseGrid = (grid) => {
+const parseGrid = (grid: string) => {
 	// Convert grids to a Map of possible values
 	const values = new Map();
 	squares.map(s => {values.set(s, digits)});
