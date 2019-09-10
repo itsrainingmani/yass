a10a757b783e2c50dfbff8d5792a32c5afbbcca5
Manikandan <tsmanikandan@protonmail.com>
2019-09-10 09:36:03 -0400

    Add comments to eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
 	parser: '@typescript-eslint/parser', // specifies the eslint parser
 	plugins: ['@typescript-eslint'],
 	extends: [
-		"plugin:@typescript-eslint/recommended",
-		"prettier/@typescript-eslint",
+		"plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		"prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
 		"plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 	],
 	parserOptions: {
