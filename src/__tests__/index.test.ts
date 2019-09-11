b46f52d4b846764713ad9e2f52682e8f6f4c2dc6
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-09-11 15:21:24 -0400

    Display start config of grid before the final result in tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -82,11 +82,13 @@ describe('Sudoku Solving Tests', () => {
 
 	it('Easy Puzzle - 1', () => {
 		const grid = '003020600900305001001806400008102900700000008006708200002609500800203009005010300';
+		yass.display(yass.gridValues(grid));
 		yass.display(yass.parseGrid(grid));
 	});
 
 	it('Hard Puzzle - 1', () => {
 		const grid = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......';
+		yass.display(yass.gridValues(grid));
 		yass.display(yass.parseGrid(grid));
 	});
 });
