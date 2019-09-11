4b3784d76d33e5ce36aadd2503fadecbc5da7940
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-09-11 14:37:52 -0400

    move units and peers of c2 checks to a nested test function

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -25,40 +25,42 @@ describe('Sudoku', () => {
 		});
 	});
 
-	it('Units of C2', () => {
-		assert.deepStrictEqual(yass.units.get('C2'), [
-			['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
-			['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
-			['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'],
-		]);
-	});
+	describe('value checks', () => {
+		it('Units of C2', () => {
+			assert.deepStrictEqual(yass.units.get('C2'), [
+				['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
+				['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
+				['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'],
+			]);
+		});
 
-	it('Peers of C2', () => {
-		assert.deepStrictEqual(
-			yass.peers.get('C2'),
-			new Set([
-				'A2',
-				'B2',
-				'D2',
-				'E2',
-				'F2',
-				'G2',
-				'H2',
-				'I2',
-				'C1',
-				'C3',
-				'C4',
-				'C5',
-				'C6',
-				'C7',
-				'C8',
-				'C9',
-				'A1',
-				'A3',
-				'B1',
-				'B3',
-			])
-		);
+		it('Peers of C2', () => {
+			assert.deepStrictEqual(
+				yass.peers.get('C2'),
+				new Set([
+					'A2',
+					'B2',
+					'D2',
+					'E2',
+					'F2',
+					'G2',
+					'H2',
+					'I2',
+					'C1',
+					'C3',
+					'C4',
+					'C5',
+					'C6',
+					'C7',
+					'C8',
+					'C9',
+					'A1',
+					'A3',
+					'B1',
+					'B3',
+				])
+			);
+		});
 	});
 
 	it('Check grid values', () => {
@@ -69,7 +71,6 @@ describe('Sudoku', () => {
 
 	it('display formatted grid', () => {
 		const grid = '003020600900305001001806400008102900700000008006708200002609500800203009005010300';
-		const parsedGrid = yass.parseGrid(grid);
-		// parsedGrid.forEach((v, k) => console.log(k, v));
+		yass.display(yass.parseGrid(grid));
 	})
 });
