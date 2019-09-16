4876d503d733728cd3628167421ceb1becdea91f
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-09-16 12:48:22 -0400

    Use array access to get the yass class private members for testing

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -9,13 +9,13 @@ describe('Yass Class Tests', () => {
 
 	describe('length checks', () => {
 		it('length of squares should be 81', () => {
-			assert.strictEqual(yass.squares.length, 81);
+			assert.strictEqual(yass["squares"].length, 81);
 		});
 		it('length of unitlist should be 27', () => {
-			assert.strictEqual(yass.unitlist.length, 27);
+			assert.strictEqual(yass["unitlist"].length, 27);
 		});
 		it('length of peers should be 20', () => {
-			const peerValue = yass.peers.get('C2');
+			const peerValue = yass["peers"].get('C2');
 			if (peerValue === undefined) {
 				return false;
 			}
@@ -25,13 +25,13 @@ describe('Yass Class Tests', () => {
 
 	describe('type checks', () => {
 		it('unitlist is an array', () => {
-			assert.strictEqual(Array.isArray(yass.unitlist), true);
+			assert.strictEqual(Array.isArray(yass["unitlist"]), true);
 		});
 	});
 
 	describe('value checks', () => {
 		it('Units of C2', () => {
-			assert.deepStrictEqual(yass.units.get('C2'), [
+			assert.deepStrictEqual(yass["units"].get('C2'), [
 				['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
 				['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
 				['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'],
@@ -40,7 +40,7 @@ describe('Yass Class Tests', () => {
 
 		it('Peers of C2', () => {
 			assert.deepStrictEqual(
-				yass.peers.get('C2'),
+				yass["peers"].get('C2'),
 				new Set([
 					'A2',
 					'B2',
@@ -82,13 +82,13 @@ describe('Sudoku Solving Tests', () => {
 	it('Easy Puzzle - 1', () => {
 		const grid = '003020600900305001001806400008102900700000008006708200002609500800203009005010300';
 		yass.display(yass.gridValues(grid));
-		yass.display(yass.parseGrid(grid));
+		yass.display(yass["parseGrid"](grid));
 	});
 
 	it('Hard Puzzle - 1', () => {
 		const grid = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......';
 		yass.display(yass.gridValues(grid));
-		yass.display(yass.parseGrid(grid));
+		yass.display(yass["parseGrid"](grid));
 	});
 
 	it('Hard Puzzle - 1 - Search', () => {
@@ -121,7 +121,7 @@ describe('DFS Test', () => {
 		yass.display(yass.solve(grid));
 
 		let globalStateGraph = yass.transitions;
-		for (let g of globalStateGraph.values()){
+		for (let g of globalStateGraph.values()) {
 			yass.display(g);
 		}
 	})
