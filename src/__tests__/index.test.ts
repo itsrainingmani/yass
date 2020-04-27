d973bb3a15f5e75780d2fa91f086706b2186f270
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-04-27 11:43:44 -0500

    Prettier changes for quotes

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -9,29 +9,29 @@ describe('Yass Class Tests', () => {
 
 	describe('length checks', () => {
 		it('length of squares should be 81', () => {
-			assert.strictEqual(yass["squares"].length, 81);
+			assert.strictEqual(yass['squares'].length, 81);
 		});
 		it('length of unitlist should be 27', () => {
-			assert.strictEqual(yass["unitlist"].length, 27);
+			assert.strictEqual(yass['unitlist'].length, 27);
 		});
 		it('length of peers should be 20', () => {
-			const peerValue = yass["peers"].get('C2');
+			const peerValue = yass['peers'].get('C2');
 			if (peerValue === undefined) {
 				return false;
 			}
 			assert.strictEqual(peerValue.size, 20);
-		})
+		});
 	});
 
 	describe('type checks', () => {
 		it('unitlist is an array', () => {
-			assert.strictEqual(Array.isArray(yass["unitlist"]), true);
+			assert.strictEqual(Array.isArray(yass['unitlist']), true);
 		});
 	});
 
 	describe('value checks', () => {
 		it('Units of C2', () => {
-			assert.deepStrictEqual(yass["units"].get('C2'), [
+			assert.deepStrictEqual(yass['units'].get('C2'), [
 				['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
 				['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
 				['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'],
@@ -40,7 +40,7 @@ describe('Yass Class Tests', () => {
 
 		it('Peers of C2', () => {
 			assert.deepStrictEqual(
-				yass["peers"].get('C2'),
+				yass['peers'].get('C2'),
 				new Set([
 					'A2',
 					'B2',
@@ -87,7 +87,8 @@ describe('Yass Class Tests', () => {
 
 	it('Grid is a multiline string', () => {
 		const singleLineGrid = '003020600900305001001806400008102900700000008006708200002609500800203009005010300';
-		const multiLineGrid = '003020600\n900305001\n001806400\n008102900\n700000008\n006708200\n002609500\n800203009\n005010300';
+		const multiLineGrid =
+			'003020600\n900305001\n001806400\n008102900\n700000008\n006708200\n002609500\n800203009\n005010300';
 		const parsedGrid1 = yass.gridValues(singleLineGrid);
 		const parseGrid2 = yass.gridValues(multiLineGrid);
 		assert.deepEqual(parsedGrid1, parseGrid2);
@@ -102,13 +103,13 @@ describe('Sudoku Solving Tests', () => {
 	it('Easy Puzzle - 1', () => {
 		const grid = '003020600900305001001806400008102900700000008006708200002609500800203009005010300';
 		yass.display(yass.gridValues(grid));
-		yass.display(yass["parseGrid"](grid));
+		yass.display(yass['parseGrid'](grid));
 	});
 
 	it('Hard Puzzle - 1', () => {
 		const grid = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......';
 		yass.display(yass.gridValues(grid));
-		yass.display(yass["parseGrid"](grid));
+		yass.display(yass['parseGrid'](grid));
 	});
 
 	it('Hard Puzzle - 1 - Search', () => {
@@ -140,9 +141,9 @@ describe('DFS Test', () => {
 		yass.display(yass.gridValues(grid));
 		yass.display(yass.solve(grid));
 
-		let globalStateGraph = yass.transitions;
-		for (let g of globalStateGraph.values()) {
+		const globalStateGraph = yass.transitions;
+		for (const g of globalStateGraph.values()) {
 			yass.display(g);
 		}
-	})
+	});
 });
